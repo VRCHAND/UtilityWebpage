495a81448951949599727be0553e7d6a2d6c25c5
vrchand2497 <vrchand2497@gmail.com>
2021-09-11 19:38:34 +0530

    ip address added

--- options.js
+++ options.js
@@ -1,6 +1,7 @@
 const optionsDisplay = document.querySelector('#show-display');
 const clickButton = document.querySelector('#myFunction');
 const span = document.getElementsByClassName('close')[0];
+const ipValue = document.querySelector('#ip');
 clickButton.addEventListener('click', () => {
     clickButton.style.display = 'none';
     optionsDisplay.style.display = 'block';
@@ -9,3 +10,6 @@ span.onclick = () => {
     optionsDisplay.style.display = 'none';
     clickButton.style.display = 'block';
 }
+fetch('https://api.ipify.org/?format=json')
+    .then((response) => response.json())
+    .then((data) => ipValue.innerHTML = data.ip);
